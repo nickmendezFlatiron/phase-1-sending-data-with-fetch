13d797062b4c3e2deed24280ed6c20b9cdc00383
Nicholas Mendez <mendez.nicholasr@gmail.com>
2022-02-09 22:08:45 -0500

    commit tree cb72c8dc59540a531108a561a90fcaa86032d193

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your code here
+function submitData(userName,userEmail){
+  return fetch('http://localhost:3000/users', {
+    method : 'POST',
+    headers : {
+      'Content-Type' : 'application/json',
+      Accept: 'application/json'
+    } ,
+    body : JSON.stringify({
+      name : `${userName}` ,
+      email : `${userEmail}`
+    })
+  })
+  .then(res => res.json())
+  .then(userObj =>{
+    document.body.innerHTML = `id : ${userObj.id}`
+  })
+  // .then(userObj =>{
+  //   const p = document.createElement('p')
+  //   p.innerText = `name : ${userObj.name}
+  //                  email : ${userObj.email}
+  //                  id : ${userObj.id}`
+  //   document.body.appendChild(p)
+  // })
+  .catch(error => {let node = document.createElement('script');
+  node.appendChild(document.createTextNode(error));
+  document.body.appendChild(node);}) 
+}
